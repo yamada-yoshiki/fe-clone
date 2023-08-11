2a6e05bab0f34145d0388feed64009ef3c0d7601
yamada-yoshiki <yama14yama0823@gmail.com>
2023-08-11 22:33:30 +0900

    Change:アドミンログイン後の遷移先を変更

--- app/controllers/admins/application_controller.rb
+++ app/controllers/admins/application_controller.rb
@@ -1,4 +1,9 @@
 class Admins::ApplicationController < ActionController::Base
   before_action :authenticate_admin!
   layout 'admins/application'
+
+  private
+  def after_sign_in_path_for(resource)
+    admins_questions_path(resource)
+  end
 end
