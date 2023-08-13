e16aaa2d51cf9e7105d88027d5bee240777addc1
yamada-yoshiki <yama14yama0823@gmail.com>
2023-08-13 16:39:57 +0900

    Add:管理者のログインのテストを追加

---
+++ spec/system/admin_sessions_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "管理者ログインテスト", type: :system do
+  it '未ログイン状態の管理画面アクセスはログイン画面に飛ぶ' do
+    visit admins_questions_path
+    expect(page).to have_current_path new_admin_session_path
+  end
+
+  it '管理者としてログイン済みの場合は管理画面にアクセスできる' do
+    admin = FactoryBot.create(:admin)
+    sign_in admin
+    visit admins_questions_path
+    expect(page).to have_current_path admins_questions_path
+    expect(page).to have_content('問題一覧')
+  end
+end
