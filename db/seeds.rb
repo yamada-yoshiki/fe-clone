99ae7a4a1a43f6ceee1445df4cb9f97e8b978afa
yamada-yoshiki <yama14yama0823@gmail.com>
2023-11-07 00:18:15 +0900

    seeds作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do |i|
+  Question.create(title: "問題#{i+1}")
+end
+
+Question.all.each do |q|
+  q.question_contents.create!(
+    sentence: "#{q.title}の問題文でーす。",
+    display_order: 0
+  )
+  4.times do |i|
+    q.question_options.create!(
+      text: "選択肢#{i}",
+      display_order: i + 1
+    )
+  end
+end
