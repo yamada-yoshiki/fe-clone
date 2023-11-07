02a846def4707ab50d616d0a4154976d60752e58
yamada-yoshiki <yama14yama0823@gmail.com>
2023-11-07 23:55:54 +0900

    questionのindexページとshowページ作成

--- app/controllers/admins/questions_controller.rb
+++ app/controllers/admins/questions_controller.rb
@@ -1,4 +1,14 @@
 class Admins::QuestionsController < Admins::ApplicationController
   def index
+    @questions = Question.all.order(:id)
+  end
+
+  def new
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    @question_contents = @question.question_contents
+    @question_options = @question.question_options
   end
 end
