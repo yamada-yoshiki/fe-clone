0de28404aa88bdf4c5b9d815e4b37772a27bac4a
yamada-yoshiki <yama14yama0823@gmail.com>
2023-11-30 23:31:53 +0900

    Change:編集をturbo_streams化

--- app/controllers/admins/question_contents_controller.rb
+++ app/controllers/admins/question_contents_controller.rb
@@ -3,8 +3,27 @@ class Admins::QuestionContentsController < Admins::ApplicationController
     @question_content = QuestionContent.new(question_id: params[:question_id])
   end
 
+  def edit
+    @question_content = QuestionContent.find(params[:id])
+  end
+
+  def update
+    # TODO:form_withでcreate,updateに振り分けてもらうようにして
+    if params[:id]
+      @question_content = QuestionContent.find(params[:id])
+      @question_content.update!(update_params)
+    else
+      @question_content = QuestionContent.new(update_params)
+      @question_content.save
+    end
+  end
+
   private
   def new_params
     params.permit(:question_id)
+  end
+
+  def update_params
+    params.require(:question_content).permit(:sentence, :display_order)
   end
 end
