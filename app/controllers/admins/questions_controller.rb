ae63a96fc7cb2bd8009e87a3fdc411b4dde65449
yamada-yoshiki <yama14yama0823@gmail.com>
2023-12-08 23:41:56 +0900

    Change:問題詳細画面の問題タイトルをモーダルで修正できるように変更

--- app/controllers/admins/questions_controller.rb
+++ app/controllers/admins/questions_controller.rb
@@ -5,6 +5,7 @@ class Admins::QuestionsController < Admins::ApplicationController
 
   def new
     @question = Question.new
+    @question_form_url = admins_questions_path
   end
 
   def create
@@ -18,6 +19,12 @@ class Admins::QuestionsController < Admins::ApplicationController
   def edit
     @question = Question.find(params[:id])
     @question_contents = @question.question_contents
+    @question_form_url = admins_question_path(@question)
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update!(update_params)    
   end
 
   def show
@@ -26,7 +33,12 @@ class Admins::QuestionsController < Admins::ApplicationController
     @question_options = @question.question_options
   end
 
+  private
   def create_params
+    params.require(:question).permit(:title)
+  end
+
+  def update_params
     params.require(:question).permit(:title)
   end
 end
