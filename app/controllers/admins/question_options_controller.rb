2545bbf7d50364e9a8549bffa8638e4d331c2d89
yamada-yoshiki <yama14yama0823@gmail.com>
2023-12-20 22:55:27 +0900

    選択肢編集をturbo_streamで実装

--- app/controllers/admins/question_options_controller.rb
+++ app/controllers/admins/question_options_controller.rb
@@ -10,6 +10,15 @@ class Admins::QuestionOptionsController < Admins::ApplicationController
     @question_option.save!
   end
 
+  def edit
+    @question_option = QuestionOption.find(params[:id])
+  end
+
+  def update
+    @question_option = QuestionOption.find(params[:id])
+    @question_option.update!(update_params)
+  end
+
   def destroy
     @question_option = QuestionOption.find(params[:id])
     @question_option.destroy
@@ -22,5 +31,9 @@ class Admins::QuestionOptionsController < Admins::ApplicationController
 
   def create_params
     params.require(:question_option).permit(:question_id, :sentence, :display_order)
+  end
+
+  def update_params
+    params.require(:question_option).permit(:sentence, :display_order)
   end
 end
