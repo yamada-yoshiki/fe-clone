55f4c7a702ed4ea33523d19f9778d66afc96cfec
yamada-yoshiki <yama14yama0823@gmail.com>
2023-12-22 23:12:22 +0900

    画像用のモーダル実装

--- app/controllers/admins/question_contents_controller.rb
+++ app/controllers/admins/question_contents_controller.rb
@@ -2,6 +2,7 @@ class Admins::QuestionContentsController < Admins::ApplicationController
   def new
     @question = Question.find(params[:question_id])
     @question_content = @question.question_contents.new
+    @modal_type = params[:type]
   end
 
   def edit
@@ -36,10 +37,10 @@ class Admins::QuestionContentsController < Admins::ApplicationController
   end
 
   def update_params
-    params.require(:question_content).permit(:sentence, :display_order)
+    params.require(:question_content).permit(:sentence, :display_order, :image_path)
   end
 
   def create_params
-    params.require(:question_content).permit(:question_id, :sentence, :display_order)
+    params.require(:question_content).permit(:question_id, :sentence, :display_order, :image_path)
   end
 end
